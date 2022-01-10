b7525aa57d4b54483f5ebf2c6bb6824af0624f48
Joseph <joseph@workit-app.com>
2022-01-10 13:42:37 +0000

    Secret Key changed and hidden

--- mixed/settings.py
+++ mixed/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
-from ..env.py import secret_key
+from .env import secret_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "10&g)tvoljrz3i_c8&jzdr@vza5lrn026q2ijfdfz4+(_la=0f")
+SECRET_KEY = os.environ.get("SECRET_KEY", secret_key)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
