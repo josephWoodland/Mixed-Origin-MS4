d52d3c4172be1ac7c17f985543128322fbad18cc
Joseph <joseph@workit-app.com>
2022-01-10 17:19:48 +0000

    Push to heroku with new secret key variable)

--- mixed/settings.py
+++ mixed/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-MY_SECRET_KEY = config('MY_SECRET_KEY')
+# MY_SECRET_KEY = config('MY_SECRET_KEY')
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'MY_SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', config('MY_SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
