d35c8e3dedc2de61e7c565db27453e63f34af34c
Joseph <joseph@workit-app.com>
2022-01-20 16:16:50 +0000

    Updated the add to cart view to deduct items from the product stock

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,6 @@
+from math import prod
 from django.shortcuts import render, redirect
-
+from products.models import Product
 # import tkinter to check the state of a checkbox
 
 # Create your views here.
@@ -10,15 +11,23 @@ def cart(request):
 
 
 def add_to_cart(request, item_id):
-
+    product = Product.objects.get(id=item_id)
     quantity = int(request.POST.get("order-amount"))
+    current_stock = product.stock_numbers
+    new_stock = current_stock - quantity
+    product.stock_numbers = new_stock
+    product.save()
+    
     redirect_url = request.POST.get("redirect_url")
     cart = request.session.get("cart", {})
+
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
     else:
         cart[item_id] = quantity
+
+
 
     request.session["cart"] = cart
     print(request.session["cart"])
