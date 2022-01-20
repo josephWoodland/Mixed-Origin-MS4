8bde4020064286f4675492b5af2075247edbb374
Joseph <joseph@workit-app.com>
2022-01-20 11:16:45 +0000

    Tested the free delivery threshold logic

--- cart/contexts.py
+++ cart/contexts.py
@@ -2,14 +2,14 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
+
 def cart_contents(request):
 
     cart_items = []
     total = 0
     product_count = 0
-    delivery_charge = settings.FREE_STANDARD_DELIVERY
-    next_day_delivery = False
-    free_delivery_threshold = 0
+    free_delivery_threshold = settings.FREE_STANDARD_DELIVERY_THRESHOLD
+    amount_to_free_standard_delivery = 0
 
     cart = request.session.get("cart", {})
 
@@ -18,17 +18,21 @@ def cart_contents(request):
         total += quantity * product.price
         product_count += quantity
 
-        cart_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-            'product_count': product_count,
-        })
+        cart_items.append(
+            {
+                "item_id": item_id,
+                "quantity": quantity,
+                "product": product,
+                "product_count": product_count,
+            }
+        )
 
-    if total < delivery_charge:
+    # Show how much to spend to get free standard delivery 
+
+    if total < free_delivery_threshold:
         delivery = 10
         free_delivery = False
-        free_delivery_threshold = delivery_charge - total
+        amount_to_free_standard_delivery = free_delivery_threshold - total
     else:
         delivery = 0
         free_delivery = True
@@ -40,10 +44,9 @@ def cart_contents(request):
         "total": total,
         "product_count": product_count,
         "free_delivery": free_delivery,
-        "next_day_delivery": next_day_delivery,
-        "delivery_charge": delivery_charge,
-        "grand_total": grand_total,
         "free_delivery_threshold": free_delivery_threshold,
+        "grand_total": grand_total,
+        "amount_to_free_standard_delivery": amount_to_free_standard_delivery,
     }
 
     return context
