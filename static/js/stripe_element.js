e03a96cddc9001e29b12ae3c8343f76bf50de11a
Joseph <joseph@workit-app.com>
2022-01-25 17:23:56 +0000

    Added the card element from stripe

--- static/js/stripe_element.js
+++ static/js/stripe_element.js
@@ -10,3 +10,20 @@ const stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 const clientSecret = $('#id_client_secret').text().slice(1, -1);
 const stripe = Stripe(stripePublicKey);
 const elements = stripe.elements();
+const style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Poppins", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#b05150',
+        iconColor: '#b05150'
+    }
+};
+const card = elements.create('card', {style: style});
+card.mount('#card-element');
