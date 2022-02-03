1764a664ddc6859ef59ebafa7839efde52d3eb96
Joseph <joseph@workit-app.com>
2022-02-03 17:04:52 +0000

    Added the email connection to google accounts - push to Heroku

--- mixed/settings.py
+++ mixed/settings.py
@@ -112,8 +112,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
 ACCOUNT_AUTHENTICATION_METHOD = "username_email"
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = "mandatory"
@@ -225,4 +223,20 @@ STRIPE_SECRET_KEY = os.environ.get(
 STRIPE_WH_SECRET = os.environ.get(
     "STRIPE_WH_SECRET_MO", config("STRIPE_WH_SECRET_MO"))
 STRIPE_CURRENCY = "gbp"
-DEFAULT_FROM_EMAIL = "order@mixed-origin.com"
+
+# Email set up
+
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.beckends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = "order@mixed-origin.com"
+
+
+else:
+    EMAIL_BACKEND = 'django.core.mail.beckends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
