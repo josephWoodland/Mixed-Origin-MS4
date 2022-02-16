201b59d4f4672bb9e87e9272799a62bdca5faf0a
Joseph <joseph@workit-app.com>
2022-02-16 15:05:32 +0000

    Set debug to False

--- mixed/settings.py
+++ mixed/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY", config("MY_SECRET_KEY", default=""))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
 
 ALLOWED_HOSTS = [
     "mixed-origin-ms4.herokuapp.com",
